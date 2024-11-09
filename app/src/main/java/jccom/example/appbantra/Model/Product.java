b135f2d822b0fc0186b92e19ff3ec034b6e5e53d
danvkph39782 <danvkph39782@fpt.edu.vn>
2024-11-09 22:12:11 +0700

    code manf home và chi tiết

---
+++ app/src/main/java/jccom/example/appbantra/Model/Product.java
@@ -0,0 +1,80 @@
+package jccom.example.appbantra.Model;
+
+public class Product {
+    private String _id;
+    private String categoryId;
+    private String name;
+    private String imageUrl;
+    private double price;
+    private String description;
+    private boolean status;
+
+    public Product(String _id, String categoryId, String name, String imageUrl, double price, String description, boolean status) {
+        this._id = _id;
+        this.categoryId = categoryId;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Product() {
+    }
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
