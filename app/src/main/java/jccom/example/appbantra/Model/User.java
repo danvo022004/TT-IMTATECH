036471d920a7ab74c22b3e4c350bedcc1d2e6176
danvkph39782 <danvkph39782@fpt.edu.vn>
2024-11-10 23:02:13 +0700

    sửa thêm tên user

--- app/src/main/java/jccom/example/appbantra/Model/User.java
+++ app/src/main/java/jccom/example/appbantra/Model/User.java
@@ -2,34 +2,17 @@ package jccom.example.appbantra.Model;
 
 public class User {
 
+    private String id;
+    private String name;
     private String phone;
     private String password;
-    private String name;
     private String email;
     private String role;
-    private String id;
 
-    public User(String phone, String password, String name, String email, String role, String id) {
+    public User(String name, String phone, String password) {
+        this.name = name;
         this.phone = phone;
         this.password = password;
-        this.name = name;
-        this.email = email;
-        this.role = role;
-        this.id = id;
-    }
-    public User(String phone, String password) {
-        this.phone = phone;
-        this.password = password;
-
-    }
-    public User(String phone, String name, String email) {
-        this.phone = phone;
-        this.name = name;
-        this.email = email;
-    }
-
-
-    public User() {
     }
 
     public String getPhone() {
@@ -40,20 +23,28 @@ public class User {
         this.phone = phone;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
@@ -70,13 +61,5 @@ public class User {
 
     public void setRole(String role) {
         this.role = role;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 }
